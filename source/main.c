4cf98343294e09dc081b21b3a75f6c67b567b562
David Perez <dpere048@wch136-05.cs.ucr.edu>
2020-05-10 19:21:49 -0700

    completed part 1

--- source/main.c
+++ source/main.c
@@ -15,7 +15,7 @@ volatile unsigned char TimerFlag = 0;
 unsigned long _avr_timer_M = 1;
 unsigned long _avr_timer_cntcurr = 0;
 unsigned char tmpA = 0x00;
-enum States{being, light1, light2, light3}state;
+enum States{begin, light1, light2, light3}state;
 void TimerOn() {
 	TCCR1B = 0x0B;
 	OCR1A = 125; 
@@ -44,13 +44,49 @@ void TimerSet(unsigned long M) {
 void LightTick(){
 	switch(state)
 	{
-		case
+		case begin:
+			state = light1;
+			break;
+		case light1:
+			state = light2;
+			break;
+		case light2
+			state = light3;
+			break;
+		case light3
+			state = light1;
+			break;
+	}
+	switch(state)
+	{
+		case light1:
+			tmpB= 0x01;
+			break;
+		case light2:
+			tmpB = 0x02;
+			break;
+		case light3:
+			tmpB = 0x04;
+			break;
+	}
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+	DDRB = 0xFF;
+	PORTB = 0x00;
+	TimerSet(1000);
+	TimerOn();
+	unsigned char tmpB = 0x00;
+	state = begin;
     /* Insert your solution below */
-    while (1) {
 
+    while (1) {
+	LightTick();
+	while (!TimerFlag);
+	TimerFlag = 0;
+	PORTB = tmpB;
+		
     }
     return 1;
 }
