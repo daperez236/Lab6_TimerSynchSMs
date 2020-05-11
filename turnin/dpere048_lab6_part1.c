1fa54d809eed17da3ef94c60f0ae8887cd2f828f
David Perez <dpere048@wch136-05.cs.ucr.edu>
2020-05-10 19:33:48 -0700

    done part 2 lab 6

--- turnin/dpere048_lab6_part1.c
+++ turnin/dpere048_lab6_part1.c
@@ -14,9 +14,8 @@
 volatile unsigned char TimerFlag = 0;
 unsigned long _avr_timer_M = 1;
 unsigned long _avr_timer_cntcurr = 0;
-unsigned char tmpC = 0x00;
 unsigned char tmpA = 0x00;
-enum States{begin, light1, pause, light2, pause2, light3, pause3}state;
+enum States{begin, light1, light2, light3}state;
 void TimerOn() {
 	TCCR1B = 0x0B;
 	OCR1A = 125; 
@@ -49,95 +48,44 @@ void LightTick(){
 			state = light1;
 			break;
 		case light1:
-			if(tmpA){
-			state = pause;
-			}
-			else{
 			state = light2;
-			}
 			break;
-		case pause:
-			if(tmpA){
-			state = light1;
-			}
-			else{
-			state = pause2;
-			}
-			break;
-		case light2:
-			if(tmpA){
-			state = pause2;
-			}
-			else{
+		case light2
 			state = light3;
-			}
 			break;
-		case pause2:
-			if(tmpA){
-			state = light2
-			}
-			else{
-			state pause2;
-			}
-		case light3:
-			if(tmpA){
-			state = pause3;
-			}
-			else{
+		case light3
 			state = light1;
-			}
-			break;
-		case pause3:
-			if(tmpA){
-			state = light3;
-			}
-			else{
-			state = pause3;
-			}
 			break;
 	}
 	switch(state)
 	{
 		case light1:
-			tmpC= 0x01;
-			break;
-		case pause:
-			tmpC = 0X01;
+			tmpB= 0x01;
 			break;
 		case light2:
-			tmpC = 0x02;
-			break;
-		case pause2:
-			tmpC = 0x02;
+			tmpB = 0x02;
 			break;
 		case light3:
-			tmpC = 0x04;
-			break;	
-		case pause3:
-			tmpC = 0x04;
+			tmpB = 0x04;
 			break;
 	}
 }
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-	DDRA = 0x00;
-	PORTA = 0xFF;
-	DDRC = 0xFF;
-	PORTC = 0x00;
-	
-	TimerSet(300);
+	DDRB = 0xFF;
+	PORTB = 0x00;
+	TimerSet(1000);
 	TimerOn();
 	unsigned char tmpB = 0x00;
 	state = begin;
     /* Insert your solution below */
 
-    while (1) {	
-	tmpA = ~PINA&0x08;
+    while (1) {
 	LightTick();
 	while (!TimerFlag);
 	TimerFlag = 0;
-	PORTC = tmpC;
+	PORTB = tmpB;
 		
     }
     return 1;
