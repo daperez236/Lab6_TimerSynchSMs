fb93e40857443929265cc6a7cb38c0240f7b6470
David Perez <dpere048@wch136-05.cs.ucr.edu>
2020-05-10 19:01:28 -0700

    part 1 started

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: dpere048
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section: 021
+ *	Assignment: Lab #6  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -11,7 +11,40 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
-
+volatile unsigned char TimerFlag = 0;
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+unsigned char tmpA = 0x00;
+enum States{being, light1, light2, light3}state;
+void TimerOn() {
+	TCCR1B = 0x0B;
+	OCR1A = 125; 
+	TIMSK1	 = 0x02; 
+	TCNT1 = 0;
+	_avr_timer_cntcurr = _avr_timer_M;
+	SREG |= 0x80;
+}
+void TimerOff() {
+	TCCR1B = 0x00;
+}
+void TimerISR(){
+	TimerFlag = 1;
+}
+ISR(TIMER1_COMPA_vect) {
+	_avr_timer_cntcurr--;
+	if (_avr_timer_cntcurr == 0){
+	TimerISR();
+	_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+void LightTick(){
+	switch(state)
+	{
+		case
 int main(void) {
     /* Insert DDR and PORT initializations */
 
